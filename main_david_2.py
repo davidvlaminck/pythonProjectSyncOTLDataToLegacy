700e643f89575f1ecbc90b067257eabedff78087
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2024-05-15 12:20:11 +0200

    specific reports

--- main_david_2.py
+++ main_david_2.py
@@ -23,4 +23,4 @@ state_db_path = Path('/home/davidlinux/Documents/AWV/resources/SyncOTLDataToLega
 if __name__ == '__main__':
     syncer = DataLegacySyncer(settings_path=settings_path, auth_type=AuthType.JWT, env=Environment.PRD,
                               state_db_path=state_db_path)
-    syncer.collect_and_create_reports()
+    syncer.collect_and_create_specific_reports(['DA-2024-06930', 'DA-2024-08481'])
